ffce8e007f49cb87a51165a35c2838e6d91b920e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-10 19:13:41 +0300

    stored strings to a variable

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,4 +2,6 @@ let currentResult = 0;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-outputResult(currentResult, '');
+let calculationDescription = '(0 + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription);
