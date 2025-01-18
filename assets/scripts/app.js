2993b7e0c1e7813f2af7e772c8b14ab68468a7e4
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-18 23:02:47 +0100

    added event listener to addBtn and changed add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult = currentResult + usrInput.value;
+  outputResult(currentResult, "");
 }
 
-currentResult = add(1, 2);
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener("click", add);
