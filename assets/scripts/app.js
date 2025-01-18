8d8431e267f4758e2c50df114e69e3024b622e8f
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-18 22:11:33 +0100

    created add function and alerted the result

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert("The result is " + result);
+}
+
+add(1, 2);
+add(5, 5);
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
