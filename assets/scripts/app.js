79fa69b8ca7caeb4dd0ee616c40630edccb47b91
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-18 21:36:58 +0100

    added a template literal expression as the value of calculationDescription variable

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,6 @@ let currentResult = defaultResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
