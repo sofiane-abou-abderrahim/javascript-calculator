d0c188e9695a0f9a8d29315d6367732a31e7704f
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-18 22:18:51 +0100

    returned value of add function with return

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,13 +3,10 @@ let currentResult = defaultResult;
 
 function add(num1, num2) {
   const result = num1 + num2;
-  alert("The result is " + result);
+  return result;
 }
 
-add(1, 2);
-add(5, 5);
-
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+currentResult = add(1, 2);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
