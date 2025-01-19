36e4a5e4ab9ec8c4dfba4e81604733ea7ed30d5f
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-19 14:09:29 +0100

    added parseInt() in add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,7 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + usrInput.value;
+  currentResult = currentResult + parseInt(usrInput.value);
   outputResult(currentResult, "");
 }
 
