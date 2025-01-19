7ec4bc1bf6e16cadac34023008965e127077a42d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-19 14:29:44 +0100

    refactored add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
 function add() {
-  currentResult = currentResult + parseInt(usrInput.value);
-  outputResult(currentResult, "");
+  const enteredNumber = getUserNumberInput();
+  const calcDescription = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener("click", add);
