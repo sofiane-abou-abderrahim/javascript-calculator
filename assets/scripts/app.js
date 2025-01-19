00b213966688cec08445c4b03be531ff4365f1b3
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-19 15:06:59 +0100

    added an array in add function and extracted it

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserNumberInput() {
   return parseInt(usrInput.value);
@@ -15,6 +16,8 @@ function add() {
   const initialResult = currentResult;
   currentResult += enteredNumber;
   createAndWriteOutput("+", initialResult, enteredNumber);
+  logEntries.push(enteredNumber);
+  console.log(logEntries);
 }
 
 function substract() {
